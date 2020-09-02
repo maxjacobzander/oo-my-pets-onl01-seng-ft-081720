28aa2bef056efa526a5dad316612d60809e35b31
maxjacobzander <maxjacobzander@gmail.com>
2020-09-02 09:37:21 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,14 +1,22 @@
 class Cat
+  attr_accessor :owner, :mood
   attr_reader :name
-  attr_writer :owner
+
+  @@all = []
 
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    @@all << self
   end
 
   def owner=(owner)
     @owner = owner
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -24,5 +24,39 @@ class Owner
   def self.reset_all
     self.all.clear
   end
-                                                                                                                                                                                                                                                                                                                                                                           
+
+  def cats
+    Cat.all.select { |cat| cat.owner == self }
+  end
+     
+  def dogs
+    Dog.all.select { |dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.each { |dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.each { |cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    Dog.all.each { |dog| dog.mood = "nervous"}
+    Dog.all.each { |dog| dog.owner = nil}
+    Cat.all.each { |cat| cat.mood = "nervous"}
+    Cat.all.each { |cat| cat.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
